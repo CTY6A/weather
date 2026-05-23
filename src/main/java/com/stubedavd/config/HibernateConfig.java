ac498d4a117b3282b6e93c3db59707882c29d495
Vadzim <stubeda0002@mail.ru>
2026-05-23 17:12:25 +0500

    create: application.properties

--- src/main/java/com/stubedavd/config/HibernateConfig.java
+++ src/main/java/com/stubedavd/config/HibernateConfig.java
@@ -6,8 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.hibernate.HibernateTransactionManager;
-import org.springframework.orm.jpa.hibernate.LocalSessionFactoryBean;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
